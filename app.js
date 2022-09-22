0486fe156b4e23840907ea7932fc48f2e62edc4c
John Doe <victoragburum@gmail.com>
2022-09-22 20:12:35 +0100

    time done

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require ("express");
+const bodyParser = require ("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+
+
+
+
+
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html")
+
+    var today  = new Date();
+   
+    var options = { 
+        weekday: "long",
+        day: "numeric",  
+        month: "long", 
+        year: "numeric",
+         };
+
+    var day  = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {kindOfDay: day});
 });
 
 app.listen(3000, function(){
