47fcb360b6bff3c6ed79b6d16b6f32553f26e288
agburumvictor <victoragburum@gmail.com>
2022-10-02 16:58:49 +0100

    sunday

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require ("express");
 const bodyParser = require ("body-parser");
 
-
 const app = express();
 
 var items = [];
@@ -9,6 +8,8 @@ var items = [];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
 
 
 app.get("/", function(req, res){
